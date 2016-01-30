086e59ebc218bcc5b626b4796d41c09298d4372e
Matt Reilly <kb1vc@kb1vc.org>
2016-01-30 14:05:11 -0500

    Bob, Alice, and the walk into the building.

--- Animation/AliceWave/do.sh
+++ Animation/AliceWave/do.sh
@@ -23,8 +23,30 @@ do
     ln -s alice0.png alice_w${an}.png
 done
 
+for (( i = 0; i < 5; i++))
+do
+    let a=$((20+i))
+    an=`printf "%03d" $a`
+    ln -s alice_sm000.png alice_w${an}.png
+done
+
+for (( i = 0; i < 5; i++))
+do
+    let a=$((25+i))
+    an=`printf "%03d" $a`
+    ln -s alice_sm001.png alice_w${an}.png
+done
+
+for (( i = 0; i < 10; i++))
+do
+    let a=$((30+i))
+    an=`printf "%03d" $a`
+    ln -s alice_sm002.png alice_w${an}.png
+done
+
+
 rm -f test.avi 
 ffmpeg -framerate 10  -i alice_w%03d.png -c:v huffyuv alice_wave.avi
 
-rm alice*.png 
+#rm alice*.png 
 
