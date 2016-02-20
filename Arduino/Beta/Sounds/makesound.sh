65547362a7802993c02a7c63151a4dee37bea30d
Matt Reilly <kb1vc@kb1vc.org>
2016-02-20 17:51:46 -0500

    updates to sound list

--- Arduino/Beta/Sounds/makesound.sh
+++ Arduino/Beta/Sounds/makesound.sh
@@ -9,7 +9,7 @@ cp space000.wav ${destdir}
 cp givecard.wav ${destdir}
 cp pokebutn.wav ${destdir}
 cp iamlost0.wav ${destdir}
-cp klaxon*.wav ${destdir}
+cp klaxon0[01].wav ${destdir}
 cp song000[0123].wav ${destdir}
 cp calmode[es].wav ${destdir}
 cp fanfare0.wav ${destdir}
